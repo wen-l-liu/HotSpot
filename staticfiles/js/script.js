3d52b19b2c3730445ba2b3c94a9b18ec0a392ef5
wen-l-liu <w.l.liu180@gmail.com>
2025-08-08 10:48:05 +0100

    collect static

---
+++ staticfiles/js/script.js
@@ -0,0 +1,8 @@
+const popoverTrigger = document.getElementById('cart-popover');
+const popoverContent = document.getElementById('cart-popover-content').innerHTML;
+
+const popover = new bootstrap.Popover(popoverTrigger, {
+    html: true,
+    content: popoverContent,
+    placement: 'bottom'
+});
